e86cc77be814da4a74fcbffc085ab3a93bf829df
rlmartinez <rlm233@msn.com>
2014-06-11 12:05:36 -0500

    Finish static pages

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,4 +39,7 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
+  #RLM - added per Chapter 2, p.19
+  config.include Capybara::DSL
 end
