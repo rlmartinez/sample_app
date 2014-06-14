f31fb30852026f2b9da4086d7cd5c085abeefb65
rlmartinez <rlm233@msn.com>
2014-06-14 11:31:29 -0500

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,34 @@
 class UsersController < ApplicationController
-  def new
+
+  def show
+    @user = User.find(params[:id])
   end
+
+ #RLM - this was not expressly deleted on p.40, Ch.7 but
+ #I think it was meant to be replaced by the user.new below.
+ #However, when I commented the next 3 lines the signup form 
+ #did not work.
+ def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)    # Not the final implementation!
+    if @user.save
+      # Handle a successful save.
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user    
+    else
+      render 'new'
+    end
+  end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+
+
 end
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "R.L. Martinez"
+    email    "rlm233@msn.com"
+    password "password"
+    password_confirmation "password"
+  end
+end
